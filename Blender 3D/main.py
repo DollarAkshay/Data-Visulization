f1c97c540386ca6cb32bf75f64096a83029812a2
Akshay.L.Aradhya <akshay95aradhya@gmail.com>
2022-06-21 18:45:00 +0530

    Adding material

--- Blender 3D/main.py
+++ Blender 3D/main.py
@@ -6,9 +6,12 @@ from perlin_noise import PerlinNoise
 import random
 import time
 
+rand_seed = 69
+# rand_seed = time.time()
+print(f"\n\nSeed: {rand_seed}")
 C = bpy.context
 D = bpy.data
-noise = PerlinNoise(8, seed=time.time())
+noise = PerlinNoise(8, seed=rand_seed)
 
 # A function to clear the scene
 
@@ -52,8 +55,8 @@ def switch_to_collection(name):
 
 # Function to setup Lighting
 def setup_lighting():
-    D.worlds["World"].node_tree.nodes["Background"].inputs[0].default_value = (0.85, 0.955, 1, 1)
-    D.worlds["World"].node_tree.nodes["Background"].inputs[1].default_value = 0.2
+    D.worlds["World"].node_tree.nodes["Background"].inputs[0].default_value = (0, 0, 0, 1)
+    D.worlds["World"].node_tree.nodes["Background"].inputs[1].default_value = 0
     C.scene.cycles.use_fast_gi = True
     C.scene.world.light_settings.ao_factor = 1
     C.scene.world.light_settings.distance = 5
@@ -71,8 +74,10 @@ def setup_lighting():
 
 # Function to setup Scene and cameras
 def setup_scene():
-    bpy.ops.mesh.primitive_plane_add(size=10000, location=(0, 0, -1))
-    bpy.ops.object.camera_add(location=(50, 50, 200), rotation=(0, 0, 0))
+    # bpy.ops.mesh.primitive_plane_add(size=10000, location=(0, 0, -1))
+
+    bpy.ops.object.camera_add(location=(-8, -5, 50),
+                              rotation=(1.0402175188064575, 0, -0.8168151378631592))
 
 
 # Main Code
@@ -84,17 +89,18 @@ setup_lighting()
 # Build up the Mesh
 start_time = time.time()
 grid_mesh = bpy.data.meshes.new('Cube Grid')
-grid_size = [100, 100, 25]
+grid_size = [150, 150, 80]
+z_limit = 4
 
 cube_size = 0.9
 verts = []
 faces = []
-
-
 for x in range(grid_size[0]):
     for y in range(grid_size[1]):
-        h = round((noise([x / grid_size[0], y / grid_size[1]]) + 1) * grid_size[2] / 2)
-        for z in range(h - 4, h):
+        perlin = (noise([x / grid_size[0], y / grid_size[1]]) + 1) / 2
+        h = round(perlin * grid_size[2])
+        h += random.randint(-round(2 * (1 - perlin)), round(2 * (1 - perlin)))
+        for z in range(h - z_limit, h):
 
             # Add the vertices of a cube from front to back
             verts.append([-cube_size / 2 + x, +cube_size / 2 + y, +cube_size / 2 + z])
@@ -126,37 +132,52 @@ C.collection.objects.link(cube_object)
 material_1 = D.materials.new('Diffuse 1')
 material_1.use_nodes = True
 bsdf = material_1.node_tree.nodes["Principled BSDF"].inputs
-bsdf['Diffuse Color'].default_value = (0.0, 0.0, 0.0, 1.0)
 bsdf['Base Color'].default_value = (0.0, 0.0, 0.0, 1.0)
-bsdf['Metallic'].default_value = 0.0
-bsdf['Roughness'].default_value = 0.0
-bsdf['Specular'].default_value = 0.0
-bsdf['Specular Tint'].default_value = 0.0
-bsdf['Subsurface'].default_value = 0.0
-bsdf['Subsurface Color'].default_value = (0.0, 0.0, 0.0, 1.0)
-bsdf['Metallic'].default_value = 0.0
+bsdf['Roughness'].default_value = 0.75
+bsdf['Specular'].default_value = 0.2
+bsdf['Anisotropic'].default_value = 0.75
 
 # Create a simple orange emission material
 material_2 = D.materials.new('Emission 1')
 material_2.use_nodes = True
 bsdf = material_2.node_tree.nodes["Principled BSDF"].inputs
 bsdf['Base Color'].default_value = (0.0, 0.0, 0.0, 1.0)
-bsdf['Emission Color'].default_value = (0.8, 0.5, 0.0, 1.0)
-bsdf['Emission'].default_value = 1.0
+bsdf['Emission'].default_value = (0.8, 0.251839, 0.0169132, 1)
+bsdf['Emission Strength'].default_value = 8
 
 # Create a simple redish-purple emission material
 material_3 = D.materials.new('Emission 2')
 material_3.use_nodes = True
 bsdf = material_3.node_tree.nodes["Principled BSDF"].inputs
 bsdf['Base Color'].default_value = (0.0, 0.0, 0.0, 1.0)
-bsdf['Emission Color'].default_value = (0.5, 0.0, 0.5, 1.0)
-bsdf['Emission'].default_value = 1.0
+bsdf['Emission'].default_value = (0.5, 0.00655472, 0.0124761, 1)
+bsdf['Emission Strength'].default_value = 8
+
 
 cube_object.data.materials.append(material_1)
+cube_object.data.materials.append(material_2)
+cube_object.data.materials.append(material_3)
 
-# modifier = cube_object.modifiers.new(name="BEVEL", type='BEVEL')
-# modifier.width = 0.05
-# modifier.segments = 4
+
+# Apply the material to random cubes
+cube_count = grid_size[0] * grid_size[1]
+
+mat_2_perc = 0.004
+cubes = random.sample(range(cube_count), round(cube_count * mat_2_perc))
+for c in cubes:
+    for i in range(6):
+        cube_object.data.polygons[c * 6 * 4 + 3 * 6 + i].material_index = 1
+
+mat_3_perc = 0.004
+cubes = random.sample(range(cube_count), round(cube_count * mat_3_perc))
+for c in cubes:
+    for i in range(6):
+        cube_object.data.polygons[c * 6 * 4 + 3 * 6 + i].material_index = 2
+
+
+modifier = cube_object.modifiers.new(name="BEVEL", type='BEVEL')
+modifier.width = 0.1
+modifier.segments = 4
 
 print(f'Mesh creation time: {time.time() - start_time:.2f} sec')
 
